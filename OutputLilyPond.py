0a696ef6b9e1a1cb5d92c205236c958cfa4f2a16
Christopher Antila <christopher.antila@mail.mcgill.ca>
2013-05-12 18:53:29 -0400

    Moved Several Methods to Module-Level Functions
    Because they are always used as functions, and there's no sense in having
    them attached to particular objects, in this case, I hope.

--- OutputLilyPond.py
+++ OutputLilyPond.py
@@ -110,6 +110,181 @@ class LilyPondObjectMaker(object):
         return self._as_m21
 
 
+def _barline_to_lily(barline):
+    """
+    Generate the LilyPond-format notation for a music21.bar.Barline object.
+
+    Parameters
+    ----------
+
+    barline : music21.bar.Barline
+        The barline to convert to LilyPond format.
+
+    Returns
+    -------
+
+    a string
+        The LilyPond notation for this barline.
+    """
+
+    # From the music21 source code... a list of barline styles...
+    #
+    # barStyleList = ['regular', 'dotted', 'dashed', 'heavy', 'double', 'final',
+    #               'heavy-light', 'heavy-heavy', 'tick', 'short', 'none']
+
+    dictionary_of_barlines = {'regular': "|", 'dotted': ":", 'dashed': "dashed",
+        'heavy': "|.|", 'double': "||", 'final': "|.", 'heavy-light': ".|",
+        'heavy-heavy': ".|.", 'tick': "'", 'short': "'", 'none': ""}
+
+    post = '\\bar "'
+
+    if barline.style in dictionary_of_barlines:
+        post += dictionary_of_barlines[barline.style] + '"'
+        return post
+    else:
+        start_msg = 'Barline type not recognized ('
+        UnidentifiedObjectError(start_msg + barline.style + ')')
+
+
+def _duration_to_lily(dur, known_tuplet=False):
+    """
+    Convert a Duration to its LilyPond-format version.
+
+    Parameters
+    ----------
+
+    dur : music21.duration.Duration
+        The duration to convert to its LilyPond-format length string.
+
+    known_tuplet : boolean
+        If we know this Duration is part of a tuplet.
+
+    Returns
+    -------
+
+    a string
+        The LilyPond string corresonding to the duration of this Duration.
+    """
+
+    # First of all, we can't deal with tuplets or multiple-component durations
+    # in this method. We need process_measure() to help.
+    if dur.tuplets is not ():
+        # We know either there are multiple components or we have part of a
+        # tuplet, we we need to find out which.
+        if len(dur.components) > 1:
+            # We have multiple components
+            raise ImpossibleToProcessError('Cannot process durations with ' +
+                'multiple components (received ' + str(dur.components) +
+                ' for quarterLength of ' + str(dur.quarterLength) + ')')
+        elif known_tuplet:
+            # We have part of a tuple. This isn't necessarily a problem; we'll
+            # assume we are given this by process_measure() and that it knows
+            # what's going on. But, in tuplets, the quarterLength doesn't match
+            # the type of written note, so we'll make a new Duration with an
+            # adjusted quarterLength
+            dur = Duration(dur.type)
+        else:
+            msg = 'duration_to_lily(): Cannot process tuplet components'
+            raise ImpossibleToProcessError(msg)
+
+    # We need both a list of our potential durations and a dictionary of what
+    # they mean in LilyPond terms.
+    list_of_durations = [16.0, 8.0, 4.0, 2.0, 1.0, 0.5, 0.25, 0.125, 0.0625, 0.03125]
+    dictionary_of_durations = {16.0: '\longa', 8.0: '\\breve', 4.0: '1', 2.0: '2', 1.0: '4',
+        0.5: '8', 0.25: '16', 0.125: '32', 0.0625: '64', 0.3125: '128'}
+
+    # So we only access the quarterLength once
+    dur_ql = dur.quarterLength
+
+    # If there are no dots, the value should be in the dictionary, and we can
+    # simply return it.
+    if dur_ql in dictionary_of_durations:
+        return dictionary_of_durations[dur_ql]
+    else:
+        # We have to figure out the largest value that will fit, then append the
+        # correct number of dots.
+        post = ''
+        for durat in list_of_durations:
+            if (dur_ql - durat) > 0.0:
+                post += dictionary_of_durations[durat]
+                break
+
+        # For every dot in this Duration, append a '.' to "post"
+        for _ in repeat(None, dur.dots):
+            post += '.'
+
+        return post
+
+
+def _octave_num_to_lily(num):
+    """
+    Calculate the LilyPond symbol corresponding to the octave number.
+
+    Parameter:
+    ----------
+
+    num : integer
+        The octave number to convert to a LilyPond register symbol, as in '4' for the octave
+        in which "middle C" occurs.
+
+    Returns:
+    --------
+
+    a string
+        A string that represents the string to append to a note to put it in the right octave.
+
+    >>> _octave_num_to_lily(1)
+    ",,"
+    >>> _octave_num_to_lily(6)
+    "'''"
+    """
+    dictionary_of_octaves = {0: ",,,", 1: ",,", 2: ",", 3: "", 4: "'", 5: "''", 6: "'''",
+        7: "''''", 8: "'''''", 9: "''''''", 10: "''''''", 11: "''''''", 12: "'''''''''"}
+
+    if num in dictionary_of_octaves:
+        return dictionary_of_octaves[num]
+    else:
+        raise UnidentifiedObjectError('Octave out of range: ' + str(num))
+
+
+def _pitch_to_lily(start_p, include_octave=True):
+    """
+    Calculate the LilyPond pitch name for the pitch.Pitch.
+
+    Parameters
+    ----------
+
+    start_p : music21.pitch.Pitch
+        The pitch to convert to its LilyPond string version.
+
+    include_octave : boolean
+        Whether to include the commas or apostrophes that indicate the absolute octave of a
+        pitch. Default is True.
+
+    Returns
+    -------
+
+    a string
+        The string-wise representation of this Pitch in LilyPond format.
+    """
+    start_pc = start_p.name.lower()
+    post = start_pc[0]
+
+    for accidental in start_pc[1:]:
+        if '-' == accidental:
+            post += 'es'
+        elif '#' == accidental:
+            post += 'is'
+
+    if include_octave:
+        if start_p.octave is None:
+            post += _octave_num_to_lily(start_p.implicitOctave)
+        else:
+            post += _octave_num_to_lily(start_p.octave)
+
+    return post
+
+
 class NoteMaker(LilyPondObjectMaker):
     """
     Class corresponding to a music21.note.Note object. Holds information about the pitch class,
@@ -145,21 +320,21 @@ class NoteMaker(LilyPondObjectMaker):
             if self._as_m21.isRest:
                 the_pitch = 'r'
             else:
-                the_pitch = NoteMaker._pitch_to_lily(self._as_m21.pitch)
+                the_pitch = _pitch_to_lily(self._as_m21.pitch)
             # But this should be the same for everybody
             for durational_component in self._as_m21.duration.components:
                 post = the_pitch
-                post += NoteMaker._duration_to_lily(durational_component, self._known_tuplet)
+                post += _duration_to_lily(durational_component, self._known_tuplet)
                 post += '~ '
             post = post[:-2]
         elif self._as_m21.isRest:
-            post += "r" + NoteMaker._duration_to_lily(self._as_m21.duration, self._known_tuplet)
+            post += "r" + _duration_to_lily(self._as_m21.duration, self._known_tuplet)
         elif hasattr(self._as_m21, 'lily_invisible') and \
         True == self._as_m21.lily_invisible:
-            post += "s" + NoteMaker._duration_to_lily(self._as_m21.duration, self._known_tuplet)
+            post += "s" + _duration_to_lily(self._as_m21.duration, self._known_tuplet)
         else:
-            post += NoteMaker._pitch_to_lily(self._as_m21.pitch) + \
-                NoteMaker._duration_to_lily(self._as_m21.duration, self._known_tuplet)
+            post += _pitch_to_lily(self._as_m21.pitch) + \
+                _duration_to_lily(self._as_m21.duration, self._known_tuplet)
 
         if self._as_m21.tie is not None:
             if self._as_m21.tie.type is 'start':
@@ -170,186 +345,12 @@ class NoteMaker(LilyPondObjectMaker):
 
         self._as_ly = post
 
-    @staticmethod
-    def _octave_num_to_lily(num):
-        """
-        Calculate the LilyPond symbol corresponding to the octave number.
-
-        Parameter:
-        ----------
-
-        num : integer
-            The octave number to convert to a LilyPond register symbol, as in '4' for the octave
-            in which "middle C" occurs.
-
-        Returns:
-        --------
-
-        a string
-            A string that represents the string to append to a note to put it in the right octave.
-
-        >>> NoteMaker._octave_num_to_lily(1)
-        ",,"
-        >>> NoteMaker._octave_num_to_lily(6)
-        "'''"
-        """
-        dictionary_of_octaves = {0: ",,,", 1: ",,", 2: ",", 3: "", 4: "'", 5: "''", 6: "'''",
-            7: "''''", 8: "'''''", 9: "''''''", 10: "''''''", 11: "''''''", 12: "'''''''''"}
-
-        if num in dictionary_of_octaves:
-            return dictionary_of_octaves[num]
-        else:
-            raise UnidentifiedObjectError('Octave out of range: ' + str(num))
-
-    @staticmethod
-    def _pitch_to_lily(start_p, include_octave=True):
-        """
-        Calculate the LilyPond pitch name for the pitch.Pitch.
-
-        Parameters
-        ----------
-
-        start_p : music21.pitch.Pitch
-            The pitch to convert to its LilyPond string version.
-
-        include_octave : boolean
-            Whether to include the commas or apostrophes that indicate the absolute octave of a
-            pitch. Default is True.
-
-        Returns
-        -------
-
-        a string
-            The string-wise representation of this Pitch in LilyPond format.
-        """
-        start_pc = start_p.name.lower()
-        post = start_pc[0]
-
-        for accidental in start_pc[1:]:
-            if '-' == accidental:
-                post += 'es'
-            elif '#' == accidental:
-                post += 'is'
-
-        if include_octave:
-            if start_p.octave is None:
-                post += NoteMaker._octave_num_to_lily(start_p.implicitOctave)
-            else:
-                post += NoteMaker._octave_num_to_lily(start_p.octave)
-
-        return post
-
-    @staticmethod
-    def _duration_to_lily(dur, known_tuplet=False):
-        """
-        Convert a Duration to its LilyPond-format version.
-
-        Parameters
-        ----------
-
-        dur : music21.duration.Duration
-            The duration to convert to its LilyPond-format length string.
-
-        known_tuplet : boolean
-            If we know this Duration is part of a tuplet.
-
-        Returns
-        -------
-
-        a string
-            The LilyPond string corresonding to the duration of this Duration.
-        """
-
-        # First of all, we can't deal with tuplets or multiple-component durations
-        # in this method. We need process_measure() to help.
-        if dur.tuplets is not ():
-            # We know either there are multiple components or we have part of a
-            # tuplet, we we need to find out which.
-            if len(dur.components) > 1:
-                # We have multiple components
-                raise ImpossibleToProcessError('Cannot process durations with ' +
-                    'multiple components (received ' + str(dur.components) +
-                    ' for quarterLength of ' + str(dur.quarterLength) + ')')
-            elif known_tuplet:
-                # We have part of a tuple. This isn't necessarily a problem; we'll
-                # assume we are given this by process_measure() and that it knows
-                # what's going on. But, in tuplets, the quarterLength doesn't match
-                # the type of written note, so we'll make a new Duration with an
-                # adjusted quarterLength
-                dur = Duration(dur.type)
-            else:
-                msg = 'duration_to_lily(): Cannot process tuplet components'
-                raise ImpossibleToProcessError(msg)
-
-        # We need both a list of our potential durations and a dictionary of what
-        # they mean in LilyPond terms.
-        list_of_durations = [16.0, 8.0, 4.0, 2.0, 1.0, 0.5, 0.25, 0.125, 0.0625, 0.03125]
-        dictionary_of_durations = {16.0: '\longa', 8.0: '\\breve', 4.0: '1', 2.0: '2', 1.0: '4',
-            0.5: '8', 0.25: '16', 0.125: '32', 0.0625: '64', 0.3125: '128'}
-
-        # So we only access the quarterLength once
-        dur_ql = dur.quarterLength
-
-        # If there are no dots, the value should be in the dictionary, and we can
-        # simply return it.
-        if dur_ql in dictionary_of_durations:
-            return dictionary_of_durations[dur_ql]
-        else:
-            # We have to figure out the largest value that will fit, then append the
-            # correct number of dots.
-            post = ''
-            for durat in list_of_durations:
-                if (dur_ql - durat) > 0.0:
-                    post += dictionary_of_durations[durat]
-                    break
-
-            # For every dot in this Duration, append a '.' to "post"
-            for _ in repeat(None, dur.dots):
-                post += '.'
-
-            return post
-
 
 class MeasureMaker(object):
     """
     Class corresponding to a music21.stream.Measure object. Holds information about all the notes
     and chords and other things in the measure.
     """
-
-    def _barline_to_lily(barline):
-        """
-        Generate the LilyPond-format notation for a music21.bar.Barline object.
-
-        Parameters
-        ----------
-
-        barline : music21.bar.Barline
-            The barline to convert to LilyPond format.
-
-        Returns
-        -------
-
-        a string
-            The LilyPond notation for this barline.
-        """
-
-        # From the music21 source code... a list of barline styles...
-        #
-        # barStyleList = ['regular', 'dotted', 'dashed', 'heavy', 'double', 'final',
-        #               'heavy-light', 'heavy-heavy', 'tick', 'short', 'none']
-
-        dictionary_of_barlines = {'regular': "|", 'dotted': ":", 'dashed': "dashed",
-            'heavy': "|.|", 'double': "||", 'final': "|.", 'heavy-light': ".|",
-            'heavy-heavy': ".|.", 'tick': "'", 'short': "'", 'none': ""}
-
-        post = '\\bar "'
-
-        if barline.style in dictionary_of_barlines:
-            post += dictionary_of_barlines[barline.style] + '"'
-            return post
-        else:
-            start_msg = 'Barline type not recognized ('
-            UnidentifiedObjectError(start_msg + barline.style + ')')
 
     def _calculate_lily(self):
         """
@@ -377,14 +378,12 @@ class MeasureMaker(object):
             self._as_m21.barDuration.quarterLength:
                 # But still, we may get something stupid...
                 try:
-                    post += "\\partial " + \
-                        NoteMaker._duration_to_lily(self._as_m21.duration) + \
-                        "\n\t"
+                    post += "\\partial " + _duration_to_lily(self._as_m21.duration) + "\n\t"
                 except UnidentifiedObjectError:
                     # ... so if it doesn't work the first time, it may in fact be a
                     # partial measure; we'll try rounding and see what we can get.
                     rounded_duration = Duration(round(self._as_m21.duration.quarterLength, 2))
-                    post += "\\partial " + NoteMaker._duration_to_lily(rounded_duration) + "\n\t"
+                    post += "\\partial " + _duration_to_lily(rounded_duration) + "\n\t"
 
         # Make self._as_m21 an iterable, so we can pull in multiple elements when we
         # need to deal with tuplets.
@@ -402,9 +401,9 @@ class MeasureMaker(object):
                 if isinstance(obj, note.Rest) and \
                 self._as_m21.srcStream.barDuration.quarterLength == obj.quarterLength:
                     if invisible:
-                        post += 's' + NoteMaker._duration_to_lily(obj.duration) + ' '
+                        post += 's' + _duration_to_lily(obj.duration) + ' '
                     else:
-                        post += 'R' + NoteMaker._duration_to_lily(obj.duration) + ' '
+                        post += 'R' + _duration_to_lily(obj.duration) + ' '
                 # Is it the start of a tuplet?
                 elif obj.duration.tuplets is not None and len(obj.duration.tuplets) > 0:
                     number_of_tuplet_components = obj.duration.tuplets[0].numberNotesActual
@@ -460,12 +459,12 @@ class MeasureMaker(object):
 
                 if 2 == len(pitch_and_mode) and pitch_and_mode[1] is not None:
                     post += "\\key " + \
-                        NoteMaker._pitch_to_lily(pitch_and_mode[0], include_octave=False) + \
+                        _pitch_to_lily(pitch_and_mode[0], include_octave=False) + \
                         " \\" + pitch_and_mode[1] + "\n\t"
                 else:
                     # We'll have to assume it's \major, because music21 does that.
                     post += "\\key " + \
-                        NoteMaker._pitch_to_lily(pitch_and_mode[0], include_octave=False) + \
+                        _pitch_to_lily(pitch_and_mode[0], include_octave=False) + \
                         " \\major\n\t"
             # Barline
             elif isinstance(obj, bar.Barline):
@@ -473,7 +472,7 @@ class MeasureMaker(object):
                 # to happen by themselves. Of course, this will have to change once
                 # we have the ability to override the standard barline.
                 if 'regular' != obj.style:
-                    post += '\n\t' + MeasureMaker._barline_to_lily(obj) + " "
+                    post += '\n\t' + _barline_to_lily(obj) + " "
             # PageLayout and SystemLayout
             elif isinstance(obj, layout.SystemLayout) or isinstance(obj, layout.PageLayout):
                 # I don't know what to do with these undocumented features.
@@ -523,10 +522,10 @@ class AnalysisVoiceMaker(object):
 
             if len(lily_this.duration.components) > 1:
                 for durational_component in lily_this.duration.components:
-                    post += NoteMaker._duration_to_lily(durational_component) + '~ '
+                    post += _duration_to_lily(durational_component) + '~ '
                     post = post[:-2]
             else:
-                post += NoteMaker._duration_to_lily(lily_this.duration)
+                post += _duration_to_lily(lily_this.duration)
 
             if lily_this.tie is not None:
                 if lily_this.tie.type is 'start':
